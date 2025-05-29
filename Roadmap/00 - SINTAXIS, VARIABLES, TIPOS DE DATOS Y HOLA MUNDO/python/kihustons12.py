b5939413f5275716761827541045003298634299
kihustons12 <131285693+kihustons12@users.noreply.github.com>
2025-05-28 21:13:10 -0500

    Create kihustons12.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/kihustons12.py
@@ -0,0 +1,55 @@
+
+# ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+# - Recuerda que todas las instrucciones de participación están en el
+#   repositorio de GitHub.
+#
+# Lo primero... ¿Ya has elegido un lenguaje?
+# - No todos son iguales, pero sus fundamentos suelen ser comunes.
+# - Este primer reto te servirá para familiarizarte con la forma de participar
+#   enviando tus propias soluciones.
+# - Lenguaje Python
+# EJERCICIO:
+# - Crea un comentario en el código y coloca la URL del sitio web oficial del
+#   lenguaje de programación que has seleccionado.
+
+#--https://www.python.org/about/
+
+# - Representa las diferentes sintaxis que existen de crear comentarios
+#   en el lenguaje (en una línea, varias...).
+
+#Una Línea 
+"""Varias Líneas"""
+
+# - Crea una variable (y una constante si el lenguaje lo soporta).
+my_variable = 1
+my_variable2 = "My_variable2"
+my_variable = "Esta es la nueva variable"
+MY_CONSTANT = "No cambiar esta constante" #Constante aún asi pueda cambiarse pero es una forma de decir que no se cambie
+
+# - Crea variables representando todos los tipos de datos primitivos
+#   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+#String
+string = "Esto es una cadena de texto"
+#int
+int = 1 #Variable de tipo entero
+#Float
+float = 2.34 # Variable decimal
+#Boulean
+boulean = False #Tipo Bouleano
+#NoneType
+NoneType = None # Valor Nulo
+#Complex
+Numero_Complejo = 2+3j
+#Bytes
+dato_binario = b'ABC'
+
+# - Imprime por terminal el texto: "¡Hola, Python!"
+print ("¡Hola, python!")
+print (boulean)
+print (Numero_Complejo)
+print (NoneType)
+print(type(Numero_Complejo))
+print(type(dato_binario))
+# ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+# debemos comenzar por el principio.
